534bd4c9364721a6174bdc164e11fc23b72660d3
Digvijay1133 <112845896+Digvijay1133@users.noreply.github.com>
2023-02-02 12:26:24 +0530

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,5 @@
+sudo yum -y install python3-pip
+git clone https://github.com/Digvijay1133/Fuel-Consumption-python.git
+cd Fuel-Consumption-python
+pip3 install -r requirements.txt
+screen -m -d python3 app.py
